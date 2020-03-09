a32ee88195c2d0630206535f1eecc70467c0cee3
Tim Reiprich <tim.reiprich@t-online.de>
2020-03-09 17:24:43 +0100

    added basic cli, possibility to rlist/delete devices, choose encryption type

--- iot.py
+++ iot.py
@@ -12,6 +12,7 @@ import paho.mqtt.client as mqtt
 import secrets
 import base64
 import random
+from datetime import datetime
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, hmac
 import os
@@ -32,127 +33,203 @@ import json
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+  print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("adios")
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+  client.subscribe("newDeviceResponse")
+  client.subscribe("authenticateResonse")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+  print(msg.topic+" "+str(msg.payload))
 
-    if msg.topic == "newDeviceResponse":
-      payload = json.loads(msg.payload)
-      getSharedKey(payload)
+  if msg.topic == "newDeviceResponse":
+    payload = json.loads(msg.payload)
+    getSharedKey(payload)
+  elif msg.topic == "authenticateResponse":
+    payload = json.loads(msg.payload)
+    authenticatePlatformAndGetTopic(payload)
+
+def authenticatePlatformAndGetTopic(payload):
+  h = hmac.HMAC(bytes.fromhex(master_key), hashes.SHA256(), backend=default_backend())
+  h.update(payload['channel'].decode())
+
+  h.verify(bytes.fromhex(payload['hmac']))
+
+  keys.update({'topic': payload['channel']})
 
 def establishConnection(client, device_id):
-    global keys
-    
-    backend = default_backend()
-    parameters = dh.generate_parameters(generator=2, key_size=512,backend=default_backend())
-    params_pem = parameters.parameter_bytes(Encoding.PEM,ParameterFormat.PKCS3)
-    print("Parámetros PKCS3: ")
-    print(params_pem.decode("utf-8"))
-    print("Parámetros Número: ")
-    print("g (alpha) = %d"%parameters.parameter_numbers().g)
-    print("p = %d\n"%parameters.parameter_numbers().p) 
+  global keys
 
-    #Generate private keys.
-    private_key = parameters.generate_private_key()
-    a_public_key = private_key.public_key()
-    print("Esta es tu clave pública: %d"%a_public_key.public_numbers().y)
+  print("Exchange new key with platform.")
+  
+  backend = default_backend()
+  parameters = dh.generate_parameters(generator=2, key_size=512,backend=default_backend())
+  params_pem = parameters.parameter_bytes(Encoding.PEM,ParameterFormat.PKCS3)
+  #print("Parámetros PKCS3: ")
+  #print(params_pem.decode("utf-8"))
+  #print("Parámetros Número: ")
+  #print("g (alpha) = %d"%parameters.parameter_numbers().g)
+  #print("p = %d\n"%parameters.parameter_numbers().p) 
 
-    data = {'g': parameters.parameter_numbers().g, 'p': parameters.parameter_numbers().p, 'public_key': a_public_key.public_numbers().y, 'id': device_id}
+  #Generate private keys.
+  private_key = parameters.generate_private_key()
+  a_public_key = private_key.public_key()
+  #print("Esta es tu clave pública: %d"%a_public_key.public_numbers().y)
 
-    payload = json.dumps(data)
-    #print(payload)
+  data = {'g': parameters.parameter_numbers().g, 'p': parameters.parameter_numbers().p, 'public_key': a_public_key.public_numbers().y, 'id': device_id}
 
-    client.subscribe("newDeviceResponse")
-    client.publish("newDevice", payload, 1)
+  payload = json.dumps(data)
+  #print(payload)
 
-    keys.update({'private_key': private_key})
+  client.publish("newDevice", payload, 1)
+
+  keys.update({'private_key': private_key})
 
 def getSharedKey(payload):
-    global keys
+  global keys
 
-    g = int(payload["g"])
-    p = int(payload["p"])
-    y = int(payload["public_key"])
+  g = int(payload["g"])
+  p = int(payload["p"])
+  y = int(payload["public_key"])
 
-    pn = dh.DHParameterNumbers(p, g)
-    parameters = pn.parameters(default_backend())
-    peer_public_numbers = dh.DHPublicNumbers(y, pn)
-    peer_public_key = peer_public_numbers.public_key(default_backend())
+  pn = dh.DHParameterNumbers(p, g)
+  parameters = pn.parameters(default_backend())
+  peer_public_numbers = dh.DHPublicNumbers(y, pn)
+  peer_public_key = peer_public_numbers.public_key(default_backend())
 
 
-    shared_key = keys['private_key'].exchange(peer_public_key)
-    print("Clave calculada por mi = %s\n"%shared_key.hex())
+  shared_key = keys['private_key'].exchange(peer_public_key)
+  #print("Clave calculada por mi = %s\n"%shared_key.hex())
 
-    derived_key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data', backend=default_backend()).derive(shared_key)
+  derived_key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data', backend=default_backend()).derive(shared_key)
 
-    keys.update({'derived_key': derived_key})
-    print(keys)
-   
+  keys.update({'derived_key': derived_key})
+  #print(keys)
+
+def runDevice(device_id, client):  
+  print('My device id is : ' + str(device_id) + "\n")
+
+  encryptions = ["fernet", "aead"]
+  choice = 0
+
+  while True:
+    print("Choose the type of encryption for this device.\n 1 fernet\n 2 aead")
+    choice = input()
+    if choice in ['1', '2']:
+      break
+
+  encryption = encryptions[int(choice) - 1]
+
+  # Inicia una nueva hebra
+  client.loop_start()
+
+  establishConnection(client, device_id)
+  #print(keys)
+
+  while 'derived_key' not in keys:
+    print("waiting for derived key")
+    time.sleep(1)
+
+  num_messages = 0
+  start_time = datetime.now()
+  current_time = datetime.now()
+  while True:
+    time_difference = current_time - start_time
+    if time_difference.seconds > 45:
+      print("Generate new key.")
+      establishConnection(client, device_id)
+    while True:
+      try:
+        num_messages=int(input("Number of messages to be sent:"))
+      except ValueError:
+        print("This is not a whole number.")
+        continue
+      break
+
+    while num_messages > 0:
+      if encryption == "fernet":
+        key = base64.urlsafe_b64encode(keys['derived_key'])
+        cipher = Fernet(key)
+        data = os.urandom(10)
+        print("The message is: " + str(data.hex()))
+        encrypted_data = cipher.encrypt(data)
+        dic = {'id': device_id, 'encryption': encryption, 'encrypted_data': encrypted_data.decode()}
+      else:
+        key = keys['derived_key']
+        cipher = ChaCha20Poly1305(key)
+        data = os.urandom(10)
+        print("The message is: " + str(data.hex()))
+        aad = b'This is additional data'
+        nonce = os.urandom(12)
+        encrypted_data = cipher.encrypt(nonce, data, aad)
+
+        #print("Encrypted data = %s\n"%encrypted_data.hex())
+
+        dic = {'id': device_id, 'encryption': encryption, 'encrypted_data': str(encrypted_data.hex()), 'nonce': str(nonce.hex()), 'aad': str(aad.hex())}
+      message = json.dumps(dic)
+
+      client.publish("messages", message, 1)
+      num_messages -= 1
+      
+      time.sleep(5)
+      current_time = datetime.now()
+
+
+def authenticate(master_key, device_id, client):
+  h = hmac.HMAC(bytes.fromhex(master_key), hashes.SHA256(), backend=default_backend())
+  h.update(bytes.fromhex(device_id))
+  code = h.finalize()
+
+  dic = {'id': device_id, 'hmac': str(code.hex())}
+  message = json.dumps(dic)
+
+  client.publish('authentication', message, 1)
+
+  print("Sent authentication message: " + message)
+
+
+def startMqtt():
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
+
+  client.username_pw_set("try","try")
+
+  client.connect("broker.shiftr.io", 1883, 60)
+  return client
+
+
+
+master_key = '03574e16832140423cc63f5ba02cd2063d3c28e41a497aa471e75fb640ca3e1c'
+
+device_id = str(os.urandom(16).hex())
+
 keys = dict()
 
-device_id = str(os.urandom(16))
-print('My device id is : ' + str(device_id))
+choice = '1'
 
-encryptions = ["fernet", "aead"]
-encryption = encryptions[0]
+while True:
+  print("Choose device type.\n 1 IoT device has neither input nor output (static master key)\n 2 IoT device has Input device\n 3 IoT device has output device\n")
+  choice = input()
+  if choice in ['1','2','3']:
+    break
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+if choice == '2':
+  print("Enter the master key (as a hex number).")
+  master_key = input()
+elif choice == '3':
+  master_key = str(os.urandom(16).hex())
+  print("Master key : " + master_key)
+  print("Enter this master key at the platform to enable authentication at the beginning of the connection.")
+  print("Press Enter when you're done.")
+  input()
+  
+client = startMqtt()
 
-client.username_pw_set("try","try")
+authenticate(master_key, device_id, client)
 
-client.connect("broker.shiftr.io", 1883, 60)
+runDevice(device_id, client)
 
-# Si quiero que esté escuchando para siempre:
-# client.loop_forever()
-# http://www.steves-internet-guide.com/loop-python-mqtt-client/
-
-# Inicia una nueva hebra
-client.loop_start()
-
-establishConnection(client, device_id)
-print(keys)
-#print("Clave calculada por mi = %s\n"%keys['shared_key'].hex())
-
-while 'derived_key' not in keys:
-  print("waiting for derived key")
-  time.sleep(1)
-
-
-if encryption == "fernet":
-  key = base64.urlsafe_b64encode(keys['derived_key'])
-  cipher = Fernet(key)
-  data = b'Test message'
-  encrypted_data = cipher.encrypt(data)
-  dic = {'id': device_id, 'encryption': encryption, 'encrypted_data': encrypted_data.decode()}
-else:
-  key = keys['derived_key']
-  cipher = ChaCha20Poly1305(key)
-  data = b'Test message'
-  aad = b'This is additional data'
-  nonce = os.urandom(12)
-  encrypted_data = cipher.encrypt(nonce, data, aad)
-
-  print("Cencrypted data = %s\n"%encrypted_data.hex())
-
-  dic = {'id': device_id, 'encryption': encryption, 'encrypted_data': str(encrypted_data.hex()), 'nonce': str(nonce.hex()), 'aad': str(aad.hex())}
-message = json.dumps(dic)
-
-while 1:
-    # Publish a message every second
-    #client.publish("hola", "Hello World", 1)
-
-    
-    print("message: " + str(message))
-
-    client.publish("messages", message, 1)
-    
-    time.sleep(10)
 
--- platform.py
+++ platform.py
@@ -31,112 +31,174 @@ import time
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+  print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("newDevice")
-    client.subscribe("removeDevice")
-    client.subscribe("messages")
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+  client.subscribe("newDevice")
+  client.subscribe("messages")
+  client.subscribe("authentication")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    if msg.topic == 'newDevice':
-      print("start DH and add new device")
-      payload = json.loads(msg.payload)
-      newDevice(client, payload)
-      print("done")
-    elif msg.topic == 'removeDevice':
-      keys.pop(msg.payload.id, 0)
-    elif msg.topic == 'messages':
-      print("got a new message")
-      payload = json.loads(msg.payload)
-      decrypt(client, payload)
-    else:
-      print(msg.topic+" "+str(msg.payload))
+  if msg.topic == 'newDevice':
+    payload = json.loads(msg.payload)
+    newDevice(client, payload)
+  elif msg.topic == 'authentication':
+    payload = json.loads(msg.payload)
+    authenticate(payload, client)
+  else:
+    print("got a new message")
+    payload = json.loads(msg.payload)
+    decrypt(client, payload)
 
 def newDevice(client, payload):
-    print("aqui")
-    g = int(payload["g"])
-    p = int(payload["p"])
-    y = int(payload["public_key"])
-    device_id = payload["id"]
+  g = int(payload["g"])
+  p = int(payload["p"])
+  y = int(payload["public_key"])
+  device_id = payload["id"]
 
-    devices.update({device_id: {}})
+  if device_id not in devices.keys():
+    print("Device needs to authenticate")
+    return 1 
 
 
-    pn = dh.DHParameterNumbers(p, g)
-    parameters = pn.parameters(default_backend())
-    peer_public_numbers = dh.DHPublicNumbers(y, pn)
-    peer_public_key = peer_public_numbers.public_key(default_backend())
+  pn = dh.DHParameterNumbers(p, g)
+  parameters = pn.parameters(default_backend())
+  peer_public_numbers = dh.DHPublicNumbers(y, pn)
+  peer_public_key = peer_public_numbers.public_key(default_backend())
 
-    private_key = parameters.generate_private_key()
+  private_key = parameters.generate_private_key()
 
-    public_key = private_key.public_key()
-    print("Esta es tu clave pública: %d"%public_key.public_numbers().y)
+  public_key = private_key.public_key()
+  #print("Esta es tu clave pública: %d"%public_key.public_numbers().y)
 
-    data = {'g': parameters.parameter_numbers().g, 'p': parameters.parameter_numbers().p, 'public_key': public_key.public_numbers().y}
-    pub_payload = json.dumps(data)
+  data = {'g': parameters.parameter_numbers().g, 'p': parameters.parameter_numbers().p, 'public_key': public_key.public_numbers().y}
+  pub_payload = json.dumps(data)
 
-    client.publish("newDeviceResponse", pub_payload, 1)
+  client.publish("newDeviceResponse", pub_payload, 1)
 
-    print("published" + pub_payload)
+  #print("published" + pub_payload)
 
-    shared_key = private_key.exchange(peer_public_key)
+  shared_key = private_key.exchange(peer_public_key)
 
-    derived_key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data', backend=default_backend()).derive(shared_key)
+  derived_key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b'handshake data', backend=default_backend()).derive(shared_key)
 
-    print("Clave calculada por mi = %s\n"%derived_key.hex())
-    devices[device_id].update({"derived_key": derived_key})
+  #print("Clave calculada por mi = %s\n"%derived_key.hex())
+  devices[device_id].update({"derived_key": derived_key})
 
 def decrypt(client, payload):
-  print("decrypt")
   device_id = payload['id']
   
-
-  
-
   if payload['encryption'] == 'fernet':
+    print("fernet")
     encrypted_data = payload['encrypted_data'].encode()
     key = base64.urlsafe_b64encode(devices[device_id]['derived_key'])
     f = Fernet(key)
     message = f.decrypt(encrypted_data)
-    print(str(message))
+
   elif payload['encryption'] == 'aead':
     print("aead")
     key = devices[device_id]['derived_key']
     aad = bytes.fromhex(payload['aad'])
     nonce = bytes.fromhex(payload['nonce'])
     encrypted_data = bytes.fromhex(payload['encrypted_data'])
-    print("aad and nonce")
+    #print("aad and nonce")
     chacha = ChaCha20Poly1305(key)
-    print("cipher")
+    #print("cipher")
     message = chacha.decrypt(nonce, encrypted_data, aad)
     
-    print("decrypted")
   else:
     print('unknown encryption')
     return 0
 
-  print(str(message))
+  print(str(message.hex()))
 
+def runDevice():
+  global master_key
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
 
+  client.username_pw_set("try","try")
 
+  client.connect("broker.shiftr.io", 1883, 60)
+
+  # Inicia una nueva hebra
+  client.loop_start()
+
+  while 1:
+    seconds = 1
+    print("What do you want to do?\n 1 Listen for new messsages\n 2 List registered devices\n 3 Remove device\n 4 Register a new device")
+    try:
+      choice1=int(input())
+    except ValueError:
+      print("This is not a whole number.")
+      continue
+    
+    if choice1 == 1:
+      try:
+        seconds = int(input("For how many seconds? "))
+      except ValueError:
+        print("This is not a whole number.")
+        continue
+      print("Listening...")
+    elif choice1 == 2:
+      print(devices.keys())
+    elif choice1 == 3:
+      device = input("Enter the id of the device that should be removed: ")
+      devices.pop(device , None)
+    elif choice1 == 4:
+      while True:
+        print("Choose device type.\n 1 IoT device has neither input nor output (static master key)\n 2 IoT device has Input device\n 3 IoT device has output device\n")
+        choice = input()
+        if choice in ['1','2','3']:
+          break
+
+      if choice == '3':
+        print("Enter the master key (as a hex number).")
+        master_key = input()
+
+      elif choice == '2':
+        master_key = str(os.urandom(16).hex())
+        print("Master key : " + master_key)
+        print("Enter this master key at the IoT device to enable authentication at the beginning of the connection.")
+
+      input("\nWaiting for authentication.\nPress Enter when device was authenticated and encryption was selected at IoT device.\n")
+
+    else:
+      print("That's not a valid choice.")
+      continue
+    time.sleep(seconds)
+
+def authenticate(payload, client):
+  h = hmac.HMAC(bytes.fromhex(master_key), hashes.SHA256(), backend=default_backend())
+  h.update(bytes.fromhex(payload['id']))
+
+  h.verify(bytes.fromhex(payload['hmac']))
+
+  devices.update({payload['id']: {}})
+  print("Device " + payload['id'] + "is authenticated and added to registered devices")
+  
+  #channel = input("\nSelect the topic for this device: ")
+  #devices[payload['id']].update({'topic': channel})
+
+  #h2 = hmac.HMAC(bytes.fromhex(master_key), hashes.SHA256(), backend=default_backend())
+  #h2.update(devices[payload['id']]['topic'].decode())
+  #channel_hash = h2.finalize()
+  
+  #dic = {'channel': devices[payload['id']]['topic'], 'hmac': str(channel_hash.hex())}
+  #print("Topic of device " + payload['id'] + "is now set to " + channel + ".")
+  #message = json.dumps(dic)
+
+  #client.publish("authenticateResponse", message, 1)
+
+master_key = '03574e16832140423cc63f5ba02cd2063d3c28e41a497aa471e75fb640ca3e1c'
 
 devices = dict()
+runDevice()
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.username_pw_set("try","try")
 
-client.connect("broker.shiftr.io", 1883, 60)
-
-# Inicia una nueva hebra
-client.loop_start()
-
-while 1:
-    time.sleep(1)
 
 
